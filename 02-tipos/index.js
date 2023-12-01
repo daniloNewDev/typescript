bf612f16908c3d169d36afbfa7ae1f3e5513cf20
Danilo Leite <danilo.cebolino@gmail.com>
2023-12-01 09:33:01 -0300

    tipos primitivos

---
+++ 02-tipos/index.js
@@ -0,0 +1,16 @@
+//  Numbers:
+var x = 10;
+x;
+console.log(typeof x);
+//  String:
+var firstName = "Danilo";
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+var lastName = "Leite";
+var fullName;
+fullName = firstName + " " + lastName;
+fullName;
+//  Boolean:
+var a = false;
+console.log(a);
+console.log(typeof a);
