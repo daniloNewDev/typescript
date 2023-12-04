fd6b0b57fecaab1f241123e23cce93d94e1089fa
Danilo Leite <danilo.cebolino@gmail.com>
2023-12-04 09:39:11 -0300

    functions

--- 05-function/01-void-callback/index.js
+++ 05-function/01-void-callback/index.js
@@ -15,3 +15,33 @@ function preGreeting(f, userName) {
 preGreeting(greeting, "Danilo");
 preGreeting(greeting, "Ruth");
 //  03- Generic Function:
+function firstElement(arr) {
+    return arr[0];
+}
+console.log(firstElement([0, 1, 2, 3, 4]));
+console.log(firstElement(["Shalom", "Danilo", "Rivka"]));
+function mergeObjects(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const newObject = mergeObjects({ name: "Danilo" }, { age: 30, job: "Developer" });
+console.log(newObject);
+//  Constraints nas Generic Functions:
+function biggestNumber(a, b) {
+    let biggest;
+    if (+a > +b) {
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    return biggest;
+}
+console.log(biggestNumber(5, 8));
+console.log(biggestNumber(14, 5));
+console.log(biggestNumber("14", "5"));
+//  Definindo tipos de parâmetros:
+function mergeArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+console.log(mergeArrays([1, 2, 3, 4], [5, 6, 7]));
+console.log(mergeArrays([1, 2, 3, 4], ["Danilo", "Lea"]));
--- 05-function/01-void-callback/index.ts
+++ 05-function/01-void-callback/index.ts
@@ -18,3 +18,42 @@ preGreeting(greeting, "Danilo");
 preGreeting(greeting, "Ruth");
 
 //  03- Generic Function:
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+console.log(firstElement([0, 1, 2, 3, 4]));
+console.log(firstElement(["Shalom", "Danilo", "Rivka"]));
+
+function mergeObjects<U, T>(obj1: U, obj2: T) {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+const newObject = mergeObjects(
+  { name: "Danilo" },
+  { age: 30, job: "Developer" }
+);
+
+console.log(newObject);
+
+//  Constraints nas Generic Functions:
+function biggestNumber<T extends number | string>(a: T, b: T): T {
+  let biggest: T;
+  if (+a > +b) {
+    biggest = a;
+  } else {
+    biggest = b;
+  }
+  return biggest;
+}
+console.log(biggestNumber(5, 8));
+console.log(biggestNumber(14, 5));
+console.log(biggestNumber("14", "5"));
+
+//  Definindo tipos de parâmetros:
+function mergeArrays<T>(arr1: T[], arr2: T[]) {
+  return arr1.concat(arr2);
+}
+console.log(mergeArrays([1, 2, 3, 4], [5, 6, 7]));
+console.log(mergeArrays<number | string>([1, 2, 3, 4], ["Danilo", "Lea"]));
